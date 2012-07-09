ad203b3ef6054a3a269ab64d3dab916c6f18784d
John Sietsma <john.sietsma@gmail.com>
2012-07-09 16:34:57 +1000

    Set device id before takeoff to solve anon user reporting.

--- unity/Assets/TestFlight/ExampleTestFlight.cs
+++ unity/Assets/TestFlight/ExampleTestFlight.cs
@@ -11,22 +11,22 @@ public class ExampleTestFlight : MonoBehaviour
         if( string.IsNullOrEmpty( token ) ) {
             Debug.LogError( "Please set your TestFlight SDK token in the editor." );
         }
-        TestFlight.TakeOff( token );
-        TestFlight.Log( "Starting TestFlight example" );
         if( Debug.isDebugBuild ) {
             TestFlight.SetDeviceID();
         }
         TestFlight.AddCustomEnvironmentInformation( "Unity Version", Application.unityVersion );
         TestFlight.AddCustomEnvironmentInformation( "System Language", Application.systemLanguage.ToString() );
+        TestFlight.TakeOff( token );
+        TestFlight.Log( "Starting TestFlight example" );
         TestFlight.PassCheckpoint( "Started TestFlight example." );
         TestFlight.SendFeedback( "This example is awesome!" );
-        TestFlight.PassCheckpoint( "About to crash" );
     }
 
     void OnGUI()
     {
         if( GUI.Button( new Rect( 20, 20, 200, 100 ), "Crash" ) ) {
             TestFlight.Log( "Crashing..." );
+            TestFlight.PassCheckpoint( "About to crash" );
             TestFlight.Crash();
         }
 
